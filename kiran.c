58bc76abf9c2899e6fe97e8489eda28b719bb954
Skywebsite <161922883+Skywebsite@users.noreply.github.com>
2024-04-06 12:09:45 +0530

    Update kiran.c

--- kiran.c
+++ kiran.c
@@ -1,15 +1,5 @@
 #include<stdio.h>
-#include<conio.h>
-void main()
-{
-char b;
-int a;
-printf("enter your name");
-scanf("%s",&b);
-printf("enter your age");
-scanf("%i",&a);
-printf("your name is %s",b);
-printf("your name is %s",b);
+int main() {
+  printf("Hello World!");
+  return 0;
 }
-
-
